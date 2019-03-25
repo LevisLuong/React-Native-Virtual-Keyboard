665aaec31df00301b42dd185cfe1e58fbd425fbc
Trung Luong <luongxuantrung@gmail.com>
2019-03-25 16:38:00 +0700

    Update VirtualKeyboard.js

--- src/VirtualKeyboard.js
+++ src/VirtualKeyboard.js
@@ -48,7 +48,8 @@ class VirtualKeyboard extends Component {
 				{this.Row([4, 5, 6])}
 				{this.Row([7, 8, 9])}
 				<View style={[styles.row, this.props.rowStyle]}>
-					{this.props.decimal ? this.Cell('.') : <View style={{ flex: 1 }} /> }
+					{this.halfCell('-')}
+					{this.props.decimal ? this.halfCell('.') : <View style={{ flex: 1 }} /> }
 					{this.Cell(0)}
 					{this.Backspace()}
 				</View>
@@ -58,7 +59,7 @@ class VirtualKeyboard extends Component {
 
 	Backspace() {
 		return (
-			<TouchableOpacity accessibilityLabel='backspace' style={styles.backspace} onPress={() => { this.onPress('back') }}>
+			<TouchableOpacity accessibilityLabel='backspace' style={[styles.backspace,this.props.cellStyle,{backgroundColor:'transparent'}]} onPress={() => { this.onPress('back') }}>
 				<Image source={this.props.backspaceImg} resizeMode='contain' style={this.props.applyBackspaceTint && ({ tintColor: this.props.color })} />
 			</TouchableOpacity>
 		);
@@ -81,6 +82,14 @@ class VirtualKeyboard extends Component {
 		);
 	}
 
+	halfCell(symbol) {
+		return (
+			<TouchableOpacity style={[styles.halfCell, this.props.cellStyle]} key={symbol} accessibilityLabel={symbol.toString()} onPress={() => { this.onPress(symbol.toString()) }}>
+				<Text style={[styles.number, { color: this.props.color }]}>{symbol}</Text>
+			</TouchableOpacity>
+		);
+	}
+	
 	onPress(val) {
 		if (this.props.pressMode === 'string') {
 			let curText = this.state.text;
